b24141d91600331ae93e2fdf8b2c584733819a1f
Jennifer Faron <jf891w@gmail.com>
2020-01-01 14:32:11 -0600

    x

---
+++ DisplayingBarChart/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Jennifer Faron
+//Assignment 3
+//Displaying a Bar Chart
+//October 26 2017
+
+namespace DisplayingBarChart
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         int number;
+         int count = 1;
+
+
+         while (count < 4)
+         {
+            Console.WriteLine("\n\nEnter a number between 1 and 30: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 30 || number == 0)
+            {
+               Console.WriteLine("This number is not between 1 and 30.\n Please reenter.");
+            }
+            else
+            {
+               for (int Asterik = 0; Asterik < number; ++Asterik)
+               {
+                  Console.Write("*");
+               }
+
+               count++;
+            }
+         }
+
+      }
+   }
+}
---
+++ Guess The Number/Guess The Number/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Jennifer Faron
+//Assignment 3
+//Guessing Game
+//October 26 2017
+
+
+namespace Guess_The_Number
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Random randomNumber = new Random();
+
+         int number = randomNumber.Next(1, 1001);
+
+         int guess;
+
+         string playAgain;
+         void NewGame()
+         {
+            do
+            {
+               Console.WriteLine("\nGuess a number between 1 and 1000: ");
+               guess = int.Parse(Console.ReadLine());
+
+               if (guess > number)
+               {
+                  Console.WriteLine("Too high! \nTry again.");
+
+               }
+               else if (guess < number)
+               {
+                  Console.WriteLine("Too low! \nTry again.");
+
+               }
+            }
+
+            while (guess != number);
+
+            Console.WriteLine("Congratulations. You guessed the number!");
+         }
+
+         NewGame();
+         Console.WriteLine("\nDo You want to play again? [Y/N]: ");
+         playAgain = Console.ReadLine().ToUpper();
+
+         if (playAgain == "Y")
+         {
+            NewGame();
+         }
+      }
+
+   }
+}
